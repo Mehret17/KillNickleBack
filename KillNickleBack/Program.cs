b69f2f2c7194f023da573f796ea6a41cef20c95e
Mehret Kulala <merutaye@gmail.com>
2018-09-13 11:26:56 -0500

    complete

--- KillNickleBack/Program.cs
+++ KillNickleBack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KillNickleBack
 {
@@ -6,7 +7,34 @@ namespace KillNickleBack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Song> goodSongs = new List<Song>();
+            HashSet<Song> allSongs = new HashSet<Song>();
+
+            allSongs.Add(new Song("Nickleback", "If today was your last day"));
+            allSongs.Add(new Song("Bryan Adams", "Please Forgive me"));
+            allSongs.Add(new Song("Taylor Swift", "Shake it off"));
+            allSongs.Add(new Song("Nickleback", "How you remind me"));
+            allSongs.Add(new Song("Nickleback", "Far away"));
+            allSongs.Add(new Song("Back Street Boys", "I want it that way"));
+            allSongs.Add(new Song("Nickleback", "How you remind me"));
+
+            foreach (var song in allSongs)
+            {
+                if (song.Artist != "Nickleback")
+                {
+                    goodSongs.Add(song);
+                }
+            }
+            Console.WriteLine("ALL THE GOOD SONGS: ");
+            Console.WriteLine();
+            foreach ( var goodsong in goodSongs)
+            {
+                Console.WriteLine($"{goodsong.Name}");
+            }
+
+            Console.ReadLine();
         }
+
     }
 }
+
---
+++ KillNickleBack/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KillNickleBack
+{
+    class Song
+    {
+        // property
+        public string Artist { get; set; }
+        public string Name { get; set; }
+
+        //  constructor
+        public Song (string artist, string name)
+        {
+            Artist = artist;
+            Name = name;
+        }
+    }
+}
